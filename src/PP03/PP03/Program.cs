b1513541a453c8cb98f334d3d8ff7cbf9fbde481
susal <susal@DESKTOP-8PBSDG0>
2023-04-18 23:36:54 +0300

    алгоритм сортировки

--- src/PP03/PP03/Program.cs
+++ src/PP03/PP03/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] arr = { 800, 11, 50, 771, 649, 770, 240, 9 };
+int temp = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr.Length - 1 - i; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
