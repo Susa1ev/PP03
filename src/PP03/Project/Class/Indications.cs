7f1e9e85a9fa20ef94158343ec3419c6c3fd9f1c
susa1ev <susalytt@gmail.com>
2023-04-19 09:15:14 +0300

    Добавление класса WeatherControl + добавление документации класса

--- src/PP03/Project/Class/Indications.cs
+++ src/PP03/Project/Class/Indications.cs
@@ -6,8 +6,29 @@ namespace Project.Class
 {
     class Indications
     {
+        /// <summary>
+        /// Температура
+        /// </summary>
         public int Temperature;
+        /// <summary>
+        /// Влажность
+        /// </summary>
         public int Humidity;
+        /// <summary>
+        /// Давление
+        /// </summary>
         public int Pressure;
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="t">Температура</param>
+        /// <param name="h">Влажность</param>
+        /// <param name="p">Давление</param>
+        Indications(int t, int h, int p)
+        {
+            Temperature = t;
+            Humidity = h;
+            Pressure = p;
+        }
     }
 }
---
+++ src/PP03/Project/Class/WeatherControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Class
+{
+    class WeatherControl
+    {
+        /// <summary>
+        /// Массив экземпляров класса типа Indications
+        /// </summary>
+        Indications[] ArrOfIndications;
+        /// <summary>
+        /// Конструктор класса. Нужен для заполнения массива
+        /// </summary>
+        /// <param name="arr">массив</param>
+        WeatherControl(Indications[] arr)
+        {
+            ArrOfIndications = arr;
+        }
+        /// <summary>
+        /// Сортировка по возрастанию
+        /// </summary>
+        void SortByAscending()
+        {
+
+        }
+    }
+}
