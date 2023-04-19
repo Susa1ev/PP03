bc8943d06791cb5aaf9e8f8a8d4c8643d97d1d44
susa1ev <susalytt@gmail.com>
2023-04-19 08:51:44 +0300

    Создание класса Indications

---
+++ src/PP03/PP03/Indications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP03
+{
+    class Indications
+    {
+        public int Temperature;
+        public int Humidity;
+        public int Pressure;
+    }
+}
